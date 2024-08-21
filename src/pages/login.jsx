a697f69e84e704e48768a88881cd177142eda4da
Grace Olafare <oluwafunhmi@gmail.com>
2024-08-21 15:11:01 -0400

    axios update

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+const axios = require("axios");
+
 function Login() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
