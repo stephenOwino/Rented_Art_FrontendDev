699cb94ff753100572c36262222a40e967ef608a
Grace Olafare <oluwafunhmi@gmail.com>
2024-08-21 14:13:27 -0400

    babel update

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+
 function Signup() {
   const [firstname, setFirstname] = useState('');
   const [lastname, setLastname] = useState('');
