cf614797a5fec60cb5e923c0215e55cfe43debb4
stephenOwino <stephenowin233@gmail.com>
2024-08-21 17:27:43 +0300

    reinstall dependencies

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
+import React, { useState } from "react";
+import axios from "axios";
 
 function Home() {
-  return (
-    <div className=" flex flex-col text-left m-4 ">
-    
-     <div className='text-3xl drop-shadow-lg text-indigo-700'>𝕽𝖊𝖓𝖙𝖊𝖉 𝕬𝖗𝖙<span className='text-red-600 text-[70px]'>.</span></div>
-     <div className='text-3xl drop-shadow-lg text-indigo-700 mt-2'> 𝓡𝓮𝓷𝓽𝓮𝓭 𝓐𝓻𝓽 </div>
-     <div className='text-3xl drop-shadow-lg text-indigo-700'>ℜ𝔢𝔫𝔱𝔢𝔡 𝔄𝔯𝔱 <span className='text-red-600 text-[70px]'>.</span></div>
-
-      
-    </div>
-  );
+	return (
+		<div className=' flex flex-col text-left m-4 '>
+			<div className='text-3xl drop-shadow-lg text-indigo-700'>
+				𝕽𝖊𝖓𝖙𝖊𝖉 𝕬𝖗𝖙<span className='text-red-600 text-[70px]'>.</span>
+			</div>
+			<div className='text-3xl drop-shadow-lg text-indigo-700 mt-2'>
+				{" "}
+				𝓡𝓮𝓷𝓽𝓮𝓭 𝓐𝓻𝓽{" "}
+			</div>
+			<div className='text-3xl drop-shadow-lg text-indigo-700'>
+				ℜ𝔢𝔫𝔱𝔢𝔡 𝔄𝔯𝔱 <span className='text-red-600 text-[70px]'>.</span>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
